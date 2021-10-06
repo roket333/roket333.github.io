636355cba5b65286d20b94096d0868b709e6a233
roket gamer <roketminecrafter@gmail.com>
2021-10-06 01:48:22 -0700

    more splashes, added comments, and some other stuff too

--- splashes.js
+++ splashes.js
@@ -1,14 +1,11 @@
 window.onload = function() {
-    splashes();
+  splashes();
 }
 
 function splashes() {
-    fetch('splashes.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
+  fetch('splashes.json').then((response) => { return response.json(); }).then((data) => {
     var random = Math.floor(Math.random() * (data.length));
     document.getElementById('splash').innerHTML = data[random];
+    //get the json, then with the data, make a random number up to it's length, then set the HTML element with the id "splash" as a random line from the json data
   });
 }
