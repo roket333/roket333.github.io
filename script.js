a31332df296bb7d0b0c89b57e76f86c619377fba
roket gamer <roketminecrafter@gmail.com>
2021-10-08 23:50:39 -0700

    added blog posts, new splashes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   splashes();
   document.getElementById("default").click();
+  loadblogposts();
 }
 
 function openTab(event, tab) {
@@ -12,28 +13,19 @@ function openTab(event, tab) {
     tabcont[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
+  //get all elements with the class "tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
+  //show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tab).style.display = "block";
   event.currentTarget.className += " active";
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function rotation(event) {
-  event.currentTarget.className += " spinning";
-  sleep(5000).then(() => { event.currentTarget.className.replace(" spinning", "") });
-}
-
 function splashes() {
-  fetch('splashes.json').then((response) => { return response.json(); }).then((data) => {
+  fetch("./splashes.json").then((splashes) => { return splashes.json(); }).then((data) => {
     var random = Math.floor(Math.random() * (data.length));
     //get the json, then with the data, make a random number up to it's length, then set the HTML element with the id "splash" as a random line from the json data
     if (random == 357) {
@@ -44,5 +36,66 @@ function splashes() {
     } else splashes();
     //if the splash is the one that shouldn't appear, try again
     //document.getElementById('splash').innerHTML = data[313]; //debug the non-appearing splash
+  });
+}
+
+function loadblogposts() {
+  var postDateString;
+  fetch("./blog/posts.json").then((posts) => { return posts.json(); }).then((data) => {
+    //get the json of blog posts
+
+    if (data.length == 0) {
+      let postdiv = document.createElement("div");
+      postdiv.classList.add("blogpost");
+      //make the actual post div and add the class "blogpost" to it
+      weird = document.createElement("p");
+      weird.classList.add("maintext");
+      weird.innerHTML = "This is weird and shouldn't be happening<br>There's either no blog posts or something has gone wrong"
+      postdiv.appendChild(weird);
+      document.getElementById("blogposts").appendChild(postdiv);
+      //so, somehow something weird happened and posts aren't working, so let's display this message in that case
+    } else {
+      data.forEach(post => {
+        let postdiv = document.createElement("div");
+        postdiv.classList.add("blogpost");
+        //make the actual post div and add the class "blogpost" to it
+
+        postTitle = post.title; //title
+        postDate = post.date; //date it was posted
+        postEdited = post.edited; //was it edited?
+        postEditDate = post.edited_date; //date it was edited
+        postContent = post.content; //actual content of the post
+        //for each post, get the title, date, weather or not it's been edited, edit date, and content
+
+        if (postEdited) {
+          postDateString = "Posted on " + postDate + ", edited on " + postEditDate
+        } else {
+          postDateString = "Posted on " + postDate
+        }
+        //get the post date ready, if it's been editied add that too
+
+        title = document.createElement("p");
+        title.classList.add("postitle");
+        title.innerHTML = postTitle;
+        date = document.createElement("p");
+        date.classList.add("postdate");
+        date.innerHTML = postDateString;
+        splitter = document.createElement("hr");
+        splitter.style.cssText += "border: 3px solid #e5e5e5;max-width:95%";
+        content = document.createElement("p");
+        content.classList.add("postcontent");
+        content.innerHTML = postContent;
+        //create all the text and assign it
+
+        postdiv.appendChild(title);
+        postdiv.appendChild(date);
+        postdiv.appendChild(splitter);
+        postdiv.appendChild(content);
+        //add all the text we just made to the div
+
+        document.getElementById("blogposts").appendChild(postdiv);
+        //add the post to div where they will all live happily ever after the end :)
+      });
+    }
   });
 }
